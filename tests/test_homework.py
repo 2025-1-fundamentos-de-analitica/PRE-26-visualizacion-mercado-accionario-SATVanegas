f2cb14651f6869fbe9758688717cc67a55eea6a7
SATvanegas <svanegasv@unal.edu.co>
2025-07-02 10:20:54 -0500

    solved

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -4,6 +4,20 @@ import os
 
 
 def test_homework():
-    """Test"""
+    """Test the homework."""
 
-    assert os.path.exists("files/output/stocks.png")
+    assert os.path.exists(
+        "files/data/demanda-comercial-dias.csv"
+    ), "files/data/demanda-comercial-dias.csv file is missing"
+
+    assert os.path.exists(
+        "files/plots/demanda-comercial-patrones-ejemplo.png"
+    ), "files/plots/demanda-comercial-patrones-ejemplo.png file is missing"
+
+    assert os.path.exists(
+        "files/plots/demanda-comercial-perfiles.png"
+    ), "files/plots/demanda-comercial-perfiles.png file is missing"
+
+    assert os.path.exists(
+        "files/plots/demanda-comercial.png"
+    ), "files/plots/demanda-comercial.png file is missing"
